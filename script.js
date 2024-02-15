b2fc9fff4d310a3fba84dabdb8d1bc1beb4ff3c6
Gabrielarndt <gabrielharndt@hotmail.com>
2024-02-15 14:17:45 -0300

    inner

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const visa = document.getElementById('visa');
 const mastercard = document.getElementById('mastercard');
 const maestro = document.getElementById('maestro');
 
-// Adicionar evento de clique aos cartões
 visa.addEventListener('click', function () {
     toggleSelection(visa);
 });
@@ -15,13 +14,10 @@ maestro.addEventListener('click', function () {
     toggleSelection(maestro);
 });
 
-// Função para alternar a seleção do cartão
 function toggleSelection(card) {
-    // Remover a classe de seleção dos cartões
     const selectedCard = document.querySelector('.selected');
     if (selectedCard) {
         selectedCard.classList.remove('selected');
     }
-    // Adicionar a classe de seleção ao cartão clicado
     card.classList.add('selected');
 }
